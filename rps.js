a3e867393f7dc9a2b19b469f7b5a15af905bc565
halfJobRob <86534221+halfJobRob@users.noreply.github.com>
2025-06-21 22:07:58 +0100

    Update rps.js
    rock paper scissor game with UI buttons, almost working

--- rps.js
+++ rps.js
@@ -1,64 +1,95 @@
-console.log("Let's Play ROCK PAPER SCISSOR");
-
-let randomNumber = Math.random();
-
-let computerChoice = getComputerChoice();
-let humanAnswer;
-let humanChoice;
-
-let computerScore = 0;
 let humanScore = 0;
+let computerScore = 0;
+let round = 0; //keep track of rounds
+
+// ComputerChoice
+const getComputerChoice = function () {
+const randomNumber = Math.floor(Math.random() * 3);
+ if (randomNumber === 0) {
+   // Check for 0
+   return "rock";
+ } else if (randomNumber === 1) {
+   return "paper";
+ } else {
+   // randomNumber will be 2 here
+   return "scissor";
+ }
+};
+// end getComputerChoice
 
 
-function getComputerChoice() {
-    if (randomNumber < 0.33) {
-    return 'rock';
-    } else if (randomNumber > 0.33 & randomNumber < 0.66) {
-        return 'paper';
-    } else {
-        return 'scissor';
-    }
-}
+// HumanChoice
+const buttons = document.querySelectorAll("button");
+const choicesDiv = document.querySelector(".choices"); //select or target choices
 
+//create a new h2 element for human and comp
+const dispHmnChoice = document.createElement("h2");
+const dispCpuChoice = document.createElement("h2");
 
-function getHumanChoice() {
-    humanAnswer = prompt("Choose rock, paper or scissor", "type rock, paper or scissor here");
-    humanChoice = humanAnswer.toLowerCase();
-}
+//set text for choices
+dispHmnChoice.textContent = "Human: ???";
+dispCpuChoice.textContent = "Computer: ???";
 
+//append display choices to choices
+choicesDiv.appendChild(dispHmnChoice);
+choicesDiv.appendChild(dispCpuChoice);
 
-function playRound() {
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const humanChoice = button.id;
+    const computerChoice = getComputerChoice(); // compChoice function must be here to be called by event listener
 
-    getHumanChoice();
-    console.log(humanChoice);
+    playRound(humanChoice, computerChoice);
 
-    getComputerChoice();
-    console.log(computerChoice);
+    //target scores
+    const dispHmnScore = document.querySelector(".human-score h2");
+    const dispCpuScore = document.querySelector(".computer-score h2");
+    //target round
+    const dispRound = document.querySelector(".round h2");
 
-
-    if (computerChoice === humanChoice) {
-        alert('DRAW');
+    // update text content of the EXISTING choice elements
+    dispHmnChoice.textContent = "Human: " + humanChoice;
+    dispCpuChoice.textContent = "Computer: " + computerChoice;
     
-    } else if (
-        (humanChoice === 'rock' && computerChoice === 'scissor') || 
-        (humanChoice === 'paper' && computerChoice === 'rock') || 
-        (humanChoice === 'scissor' && computerChoice === 'paper')
-    ) { 
-        alert('Human WINS') & (humanScore = humanScore +1);
+    // update text content
+    dispHmnScore.textContent = "You: " + humanScore;
+    dispCpuScore.textContent = "Machine: " + computerScore;
+    dispRound.textContent = "Round: " + round;
 
+    if (humanScore >= 5 || computerScore >= 5) {
+      endGame();
+    }
 
+  });
+})
+
+function endGame() {
+    if (humanScore >= 5) {
+     // parentNode.removeChild(child);
+      alert("You Win!");
     } else {
-        alert('Computer WINS') & (computerScore = computerScore +1);
-        
+     // parentNode.removeChild(child);
+      alert("You Lose!");
     }
 }
 
+function playRound(humanSelection, computerSelection) {
 
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        playRound();
-    }
-    alert('Human: ' + humanScore + ' Computer: ' + computerScore);
-}
+           if (humanSelection === computerSelection) {
+           } else if ((humanSelection === "rock" && computerSelection === "scissor") || 
+                  (humanSelection === "paper" && computerSelection === "rock") || 
+                  (humanSelection === "scissor" && computerSelection === "paper")) {
+             humanScore++;
+           } else {
+             computerScore++;
+           }
+           console.log(humanSelection);
+           console.log(computerSelection);
+           round++;
+         }
 
-playGame();
+// !logic complete
+
+// Create a function playGame
+// Move playRound and scores inside playGame
+// Play 5 rounds
